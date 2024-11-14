a4acf32d249d6b1ddb9daf6326b0dbb21999be7d
Meik Truschkowski <meik.truschkowski@btelligent.com>
2024-11-14 12:58:09 +0100

    Diagrame und MD erstmalig im batch generiert

--- drawio/readme.md
+++
@@ -1,5 +0,0 @@
-# Draw.io Source Files
-
-In this folder the origninal draw.io files are stored. 
-
-Then can be saved directly from the draw.io website using "Save as..." / "Speichern unter..."
--- extract_drawio.py
+++ extract_drawio.py
@@ -1,5 +1,7 @@
 import xml.etree.ElementTree as ET
 import argparse
+import subprocess
+import yaml
 import os
 
 def extract_information(xml_path):
@@ -60,6 +62,35 @@ def extract_style_value(style, key):
     # Helper function to extract values from the style attribute
     style_dict = dict(item.split('=') for item in style.split(';') if '=' in item)
     return style_dict.get(key, 'None')
+
+def load_config(yaml_path):
+
+    with open(yaml_path, 'r') as f:
+        config = yaml.safe_load(f)
+    return config
+
+def export_drawio_to_png(drawio_executable, input_file, output_file):
+    # Stellen Sie sicher, dass das Verzeichnis für die Ausgabe existiert
+    output_dir = os.path.dirname(output_file)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    # Konstruktion des Befehls
+    command = [
+        drawio_executable, 
+        "--export", 
+        "--format", "png", 
+        "--output", output_file, 
+        input_file
+    ]
+
+    # Ausführung des Befehls
+    try:
+        subprocess.run(command, check=True)
+        print(f"Export erfolgreich: {output_file}")
+    except subprocess.CalledProcessError as e:
+        print(f"Fehler beim Exportieren: {e}")
+
     
 def to_markdown_file(xml_path, shapes, connections):
     # Generate markdown filename
@@ -79,30 +110,70 @@ def to_markdown_file(xml_path, shapes, connections):
             md_file.write(f"- **ID:** {connection['ID']}, **Label:** {connection['Label']}, **Source:** {connection['Source']}, **Target:** {connection['Target']}, **ADAPT Type:** {connection['ADAPT Type']}, **Start Arrow:** {connection['Start Arrow']}, **End Arrow:** {connection['End Arrow']}\n")
                                     
         md_file.write(f"\n\n### und hier haben wir auch das Diagramm dazu:\n")
-        md_file.write(f"![Diagramm {base_name}]({base_name}.png)\n")
+        md_file.write(f"![Diagramm {base_name}](png\{base_name}.png)\n")
 
     print(f"Markdown file '{md_filename}' has been created.")
 
 def main():
-    parser = argparse.ArgumentParser(description='Extract draw.io shape and connection information from XML.')
-    parser.add_argument('xml_path', help='Path to the draw.io XML file')
-    args = parser.parse_args()
-    xml_path = args.xml_path
-
-    shapes, connections = extract_information(xml_path)
+    # Set the directory path to the "drawio" folder in the current working directory
+    drawio_directory = os.path.join(os.getcwd(), 'drawio')
+    docs_directory = os.path.join(os.getcwd(), 'docs')
+    png_directory = os.path.join(os.getcwd(), 'png')
     
-    # Print extracted shapes
-    print("Shapes (Nodes):")
-    for shape in shapes:
-        print(f"ID: {shape['ID']}, Label: {shape['Label']}, ADAPT Type: {shape['ADAPT Type']}")
+    # Laden der Konfiguration aus der YAML-Datei
+    config = load_config('config.yaml')
+    drawio_executable = config.get('drawio_executable_path')
 
-    # Print extracted connections
-    print("\nConnections (Edges):")
-    for connection in connections:
-        print(f"ID: {connection['ID']}, Label: {connection['Label']}, Source: {connection['Source']}, Target: {connection['Target']}, ADAPT Type: {connection['ADAPT Type']}, Start Arrow: {connection['Start Arrow']}, End Arrow: {connection['End Arrow']}")
-        
-    # Print extracted information
-    to_markdown_file(xml_path, shapes, connections)
+    if not drawio_executable:
+        print("Draw.io executable path is not set in the configuration file.")
+        return
+
+    # Check if the directory exists
+    if not os.path.exists(png_directory):
+        print(f"The directory '{png_directory}' does not exist.")
+        return
+
+    # Check if the directory exists
+    if not os.path.exists(drawio_directory):
+        print(f"The directory '{drawio_directory}' does not exist.")
+        return
+
+    # Check if the directory exists
+    if not os.path.exists(docs_directory):
+        print(f"The directory '{docs_directory}' does not exist.")
+        return
+
+    for root_dir, dirs, files in os.walk(drawio_directory):
+
+            for file in files:
+
+                if file.endswith('.drawio'):
+
+                    drawio_path = os.path.join(drawio_directory, file)
+                    docs_path = os.path.join(docs_directory, file)
+                    
+                    base_name = os.path.splitext(os.path.join(png_directory, file))[0]
+                    png_path = f"{base_name}.png"
+
+                    print(f"Processing file: {drawio_path}")
+                    print(f"PNG file: {png_path}")
+                    
+                    export_drawio_to_png(drawio_executable, drawio_path, png_path)
+
+                    shapes, connections = extract_information(drawio_path)
+                    
+                    # Print extracted shapes
+                    print("Shapes (Nodes):")
+                    for shape in shapes:
+                        print(f"ID: {shape['ID']}, Label: {shape['Label']}, ADAPT Type: {shape['ADAPT Type']}")
+
+                    # Print extracted connections
+                    print("\nConnections (Edges):")
+                    for connection in connections:
+                        print(f"ID: {connection['ID']}, Label: {connection['Label']}, Source: {connection['Source']}, Target: {connection['Target']}, ADAPT Type: {connection['ADAPT Type']}, Start Arrow: {connection['Start Arrow']}, End Arrow: {connection['End Arrow']}")
+                        
+                    # Print extracted information
+                    to_markdown_file(docs_path, shapes, connections)
 
 if __name__ == "__main__":
     main()
